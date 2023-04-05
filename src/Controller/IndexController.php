20864719386f47751022aded5212480e47a7cf79
badiskefi <badiskefi@gmail.com>
2023-04-06 00:16:00 +0100

    commit tp4 symfony6

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -10,8 +10,17 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
+use Doctrine\Persistence\ManagerRegistry;
+
+
 class IndexController extends AbstractController
 {
+    private $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
 /**
  *@Route("/",name="article_list")
  */
@@ -19,21 +28,102 @@ public function home()
  {
  //récupérer tous les articles de la table article de la BD
  // et les mettre dans le tableau $articles
-$articles= $this->getDoctrine()->getRepository(Article::class)->findAll();
+$articles= $this->doctrine->getRepository(Article::class)->findAll();
 return $this->render('articles/index.html.twig',['articles'=> $articles]);
  }
  /**
  * @Route("/article/save")
  */
 public function save() {
-    $entityManager = $this->getDoctrine()->getManager();
+    $entityManager = $this->doctrine->getManager();
     $article = new Article();
-    $article->setNom('Article 1');
+    $article->setName('Article 1');
     $article->setPrix(1000);
    
     $entityManager->persist($article);
     $entityManager->flush();
     return new Response('Article enregisté avec id '.$article->getId());
     }
+
+    /**
+     * @Route("/article/new", name="new_article")
+     * Method({"GET", "POST"})
+     */
+    public function new(Request $request) {
+        $article = new Article();
+        $form = $this->createFormBuilder($article)
+            ->add('nom', TextType::class)
+            ->add('prix', TextType::class)
+            ->add('save', SubmitType::class, array(
+                'label' => 'Créer'
+            ))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $article = $form->getData();
+
+            $entityManager = $this->doctrine->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('article_list');
+        }
+
+        return $this->render('articles/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+/**
+ * @Route("/article/{id}", name="article_show")
+ */
+public function show($id) {
+    $article = $this->doctrine->getRepository(Article::class)
+    ->find($id);
+    return $this->render('articles/show.html.twig',
+    array('article' => $article));
+     }
+/**
+ * @Route("/article/edit/{id}", name="edit_article")
+ * Method({"GET", "POST"})
+ */
+public function edit(Request $request, $id) {
+    $article = new Article();
+    $article = $this->doctrine->getRepository(Article::class)->find($id);
+   
+    $form = $this->createFormBuilder($article)
+    ->add('nom', TextType::class)
+    ->add('prix', TextType::class)
+    ->add('save', SubmitType::class, array(
+    'label' => 'Modifier'
+    ))->getForm();
+   
+    $form->handleRequest($request);
+    if($form->isSubmitted() && $form->isValid()) {
+   
+    $entityManager = $this->doctrine->getManager();
+    $entityManager->flush();
+   
+    return $this->redirectToRoute('article_list');
+    }
+    return $this->render('articles/edit.html.twig', ['form' => $form->createView()]);
+     }
+    
+/**
+     * @Route("/article/{id}", name="article_delete", methods={"DELETE"})
+     */
+public function delete(Request $request, $id) {
+    $article = $this->doctrine->getRepository(Article::class)->find($id);
+   
+    $entityManager = $this->doctrine->getManager();
+    $entityManager->remove($article);
+    $entityManager->flush();
+   
+    $response = new Response();
+    $response->send();
+    return $this->redirectToRoute('article_list');
+    }
+    
    
 }
