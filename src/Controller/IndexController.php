79fa1e3e1035290c2b7de11046f7eed587964292
badiskefi <badiskefi@gmail.com>
2023-04-05 14:49:55 +0100

    second commit tp2 part 2 symfony6

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,8 +2,12 @@
 namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+Use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController
 {
+ /**
+ * @Route("/")
+ */
  public function home()
  {
  return new Response('<h1>Ma première page Symfony</h1>');
