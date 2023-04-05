e17aaa00e3d4e087754e2faea046ef50ffa3cc51
badiskefi <badiskefi@gmail.com>
2023-04-05 18:06:50 +0100

    commit tp3 symfony6

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,9 +5,12 @@ use Symfony\Component\HttpFoundation\Response;
 Use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController
 {
- #[Route('/{name}')]
- public function home($name)
+ /**
+ *@Route("/")
+ */
+ public function home()
  {
-    return $this->render('index.html.twig',['name' => $name]); 
+    $articles = ['Artcile1', 'Article 2','Article 3'];
+    return $this->render('articles/index.html.twig',['articles' => $articles]);  
  }
 }
