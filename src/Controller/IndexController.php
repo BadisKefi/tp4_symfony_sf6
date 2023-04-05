a9a5807566299135385a12e0d02631301d991ae6
badiskefi <badiskefi@gmail.com>
2023-04-05 23:16:38 +0100

    commit tp4 symfony6

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,14 +3,37 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 Use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\Article;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 class IndexController extends AbstractController
 {
- /**
- *@Route("/")
+/**
+ *@Route("/",name="article_list")
  */
- public function home()
+public function home()
  {
-    $articles = ['Artcile1', 'Article 2','Article 3'];
-    return $this->render('articles/index.html.twig',['articles' => $articles]);  
+ //récupérer tous les articles de la table article de la BD
+ // et les mettre dans le tableau $articles
+$articles= $this->getDoctrine()->getRepository(Article::class)->findAll();
+return $this->render('articles/index.html.twig',['articles'=> $articles]);
  }
+ /**
+ * @Route("/article/save")
+ */
+public function save() {
+    $entityManager = $this->getDoctrine()->getManager();
+    $article = new Article();
+    $article->setNom('Article 1');
+    $article->setPrix(1000);
+   
+    $entityManager->persist($article);
+    $entityManager->flush();
+    return new Response('Article enregisté avec id '.$article->getId());
+    }
+   
 }
